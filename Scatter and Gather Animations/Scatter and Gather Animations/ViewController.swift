752f07316ecbb311f628c8c2fd48eac2375b6ba3
fritzgt <tuanis@live.com>
2020-02-14 12:17:22 +0200

    completed MVP

--- Scatter and Gather Animations/Scatter and Gather Animations/ViewController.swift
+++ Scatter and Gather Animations/Scatter and Gather Animations/ViewController.swift
@@ -10,11 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
     
- 
+    var firstLabel: UILabel!
+    var secondLabel: UILabel!
+    var thirdLabel: UILabel!
+    var forthLabel: UILabel!
+    var fifthLabel: UILabel!
+    var sixthLabel: UILabel!
+    
     var imageView: UIImageView!
     
     var isScattered = false
     
+    var positionL: CGPoint?
+    var positionA: CGPoint?
+    var positionM: CGPoint?
+    var positionB: CGPoint?
+    var positionD: CGPoint?
+    var positionAs: CGPoint?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -40,54 +52,48 @@ class ViewController: UIViewController {
         self.navigationItem.rightBarButtonItem = toggleButtonPressed
     }
     
-    //Func to toggle Bool
-    @objc func navButtonPressed() {
-        isScattered = !isScattered
-        print(isScattered)
-    }
-    
     
     //MARK: - Setup Labels
     
     func setupLabels() {
         
         //1 label
-        let firstLabel = UILabel()
+        firstLabel = UILabel()
         firstLabel.translatesAutoresizingMaskIntoConstraints = false
         firstLabel.text = "L"
         firstLabel.textAlignment = .center
         firstLabel.font = UIFont.systemFont(ofSize: 60)
 
         //2 label
-        let secondLabel = UILabel()
+        secondLabel = UILabel()
         secondLabel.translatesAutoresizingMaskIntoConstraints = false
         secondLabel.text = "A"
         secondLabel.textAlignment = .center
         secondLabel.font = UIFont.systemFont(ofSize: 60)
         
         //3 label
-        let thirdLabel = UILabel()
+        thirdLabel = UILabel()
         thirdLabel.translatesAutoresizingMaskIntoConstraints = false
         thirdLabel.text = "M"
         thirdLabel.textAlignment = .center
         thirdLabel.font = UIFont.systemFont(ofSize: 60)
         
         //4 label
-        let forthLabel = UILabel()
+        forthLabel = UILabel()
         forthLabel.translatesAutoresizingMaskIntoConstraints = false
         forthLabel.text = "B"
         forthLabel.textAlignment = .center
         forthLabel.font = UIFont.systemFont(ofSize: 60)
         
         //5 label
-        let fifthLabel = UILabel()
+        fifthLabel = UILabel()
         fifthLabel.translatesAutoresizingMaskIntoConstraints = false
         fifthLabel.text = "D"
         fifthLabel.textAlignment = .center
         fifthLabel.font = UIFont.systemFont(ofSize: 60)
         
         //6 label
-        let sixthLabel = UILabel()
+        sixthLabel = UILabel()
         sixthLabel.translatesAutoresizingMaskIntoConstraints = false
         sixthLabel.text = "A"
         sixthLabel.textAlignment = .center
@@ -134,6 +140,104 @@ class ViewController: UIViewController {
         //add to the view
         view.addSubview(imageView)
         
+    }
+    
+    
+    //MARK: - Animation
+    
+    
+    //Func to toggle Bool
+    @objc func navButtonPressed() {
+        
+        //Toggle bool
+        isScattered = !isScattered
+        
+        //animate base on bool
+        if isScattered{
+            scatter()
+        }else{
+            gather()
+        }
+    }
+    
+    
+    func scatter() {
+        
+        let animationBlock = {
+            
+            //Fade and rotate letters
+            UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 0.5){
+                //fade img
+                self.imageView.alpha = 0.0
+                
+                self.firstLabel.transform = CGAffineTransform(rotationAngle: CGFloat.random(in: 1...360))
+                self.secondLabel.transform = CGAffineTransform(rotationAngle: CGFloat.random(in: 1...360))
+                self.thirdLabel.transform = CGAffineTransform(rotationAngle: CGFloat.random(in: 1...360))
+                self.forthLabel.transform = CGAffineTransform(rotationAngle: CGFloat.random(in: 1...360))
+                self.fifthLabel.transform = CGAffineTransform(rotationAngle: CGFloat.random(in: 1...360))
+                self.sixthLabel.transform = CGAffineTransform(rotationAngle: CGFloat.random(in: 1...360))
+                
+            }
+            
+            //Move letters to another location
+            UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 0.5){
+                
+                self.positionL = self.firstLabel.center
+                self.positionA = self.secondLabel.center
+                self.positionM = self.thirdLabel.center
+                self.positionB = self.forthLabel.center
+                self.positionD = self.fifthLabel.center
+                self.positionAs = self.sixthLabel.center
+            
+                
+                self.firstLabel.center =  CGPoint(x: Int.random(in: -50...400), y: Int.random(in: -100...400))
+                self.secondLabel.center = CGPoint(x: Int.random(in: -50...400), y: Int.random(in: -100...400))
+                self.thirdLabel.center = CGPoint(x: Int.random(in: -50...400), y: Int.random(in: -100...400))
+                self.forthLabel.center = CGPoint(x: Int.random(in: -50...400), y: Int.random(in: -100...400))
+                self.fifthLabel.center = CGPoint(x: Int.random(in: -50...400), y: Int.random(in: -100...400))
+                self.sixthLabel.center = CGPoint(x: Int.random(in: -50...400), y: Int.random(in: -100...400))
+                
+            }
+        }
+        
+        
+        UIView.animateKeyframes(withDuration: 2.0, delay: 0, options: [], animations: animationBlock, completion: nil)
+    }
+    
+    
+    
+    func gather() {
+
+        let animationBlock = {
+            
+            //Fade and rotate letters
+            UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 0.5){
+                //fade img
+                self.imageView.alpha = 1.0
+                
+                self.firstLabel.transform = .identity
+                self.secondLabel.transform = .identity
+                self.thirdLabel.transform = .identity
+                self.forthLabel.transform = .identity
+                self.fifthLabel.transform = .identity
+                self.sixthLabel.transform = .identity
+                
+            }
+            
+            //Move letters to another location
+            UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 0.5){
+                
+                self.firstLabel.center = self.positionL!
+                self.secondLabel.center = self.positionA!
+                self.thirdLabel.center = self.positionM!
+                self.forthLabel.center = self.positionB!
+                self.fifthLabel.center = self.positionD!
+                self.sixthLabel.center = self.positionAs!
+                
+            }
+        }
+        
+        UIView.animateKeyframes(withDuration: 2.0, delay: 0, options: [], animations: animationBlock, completion: nil)
     }
 
 
