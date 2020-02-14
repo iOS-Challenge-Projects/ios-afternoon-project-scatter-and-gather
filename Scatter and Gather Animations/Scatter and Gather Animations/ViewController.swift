5ae7eebac2c5d6272319bd6efcf25fafe6c7f869
fritzgt <tuanis@live.com>
2020-02-14 04:30:34 +0200

    initial commit

---
+++ Scatter and Gather Animations/Scatter and Gather Animations/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Scatter and Gather Animations
+//
+//  Created by FGT MAC on 2/14/20.
+//  Copyright © 2020 FGT MAC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
